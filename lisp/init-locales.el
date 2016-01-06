95cbc6ab7cd7c8537414ef0cd5ba79237713cc11
liwenzhu <wzli@kabaminc.com>
2016-01-06 14:28:08 +0800

    menu-bar-mode -1

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -5,9 +5,9 @@
 (defun sanityinc/locale-is-utf8-p ()
   "Return t iff the \"locale\" command or environment variables prefer UTF-8."
   (or (sanityinc/utf8-locale-p (and (executable-find "locale") (shell-command-to-string "locale")))
-      (sanityinc/utf8-locale-p (getenv "LC_ALL"))
-      (sanityinc/utf8-locale-p (getenv "LC_CTYPE"))
-      (sanityinc/utf8-locale-p (getenv "LANG"))))
+	  (sanityinc/utf8-locale-p (getenv "LC_ALL"))
+	  (sanityinc/utf8-locale-p (getenv "LC_CTYPE"))
+	  (sanityinc/utf8-locale-p (getenv "LANG"))))
 
 (when (or window-system (sanityinc/locale-is-utf8-p))
   (set-language-environment 'utf-8)
@@ -17,6 +17,7 @@
   (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
   (prefer-coding-system 'utf-8))
 
+(menu-bar-mode -1)
 (setq-default tab-width 4)
 (setq-default tab-always-indent t)
 (setq-default indent-tabs-mode t)
