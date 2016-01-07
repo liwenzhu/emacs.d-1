eb78b891f05e24311994791ac1bca83ece2141d7
liwenzhu <wzli@kabaminc.com>
2016-01-07 12:19:05 +0800

    add C-c w copy word.

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -24,4 +24,25 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+(defun get-point (symbol &optional arg)
+  "get the point"
+  (funcall symbol arg)
+  (point))
+
+(defun copy-thing (begin-of-thing end-of-thing &optional arg)
+  "copy thing between beg & end into kill ring"
+  (save-excursion
+	(let ((beg (get-point begin-of-thing 1))
+		  (end (get-point end-of-thing arg)))
+	  (copy-region-as-kill beg end))))
+
+(defun copy-word (&optional arg)
+  "Copy words at point into kill-ring"
+  (interactive "P")
+  (copy-thing 'backward-word 'forward-word arg)
+  ;;(paste-to-mark arg)
+  )
+
+(global-set-key (kbd "C-c w")         (quote copy-word))
+
 (provide 'init-locales)
